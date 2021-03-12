398b881c9ca386c1a4e38ab872ce7fbd62a354b7
vundelayamini <vundelayamini@gmail.com>
2021-03-12 14:24:32 +0530

    [yamini]Add: Enter a Valid First Name

---
+++ UserRegistration/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace UserRegistration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = "Yamini";
+            Console.WriteLine("Firt name result :" + result);
+        }
+    }
+}
---
+++ UserRegistration/Regex.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserRegistration
+{
+    class Regex
+    {
+        string pattren = "^[A-Z]{1}[A-Za-z]{2,}$";
+        public bool ValidateLastName(string lastName, Regex_LastName)
+        {
+            return Regex.IsMatch(lastName, Regex_LastName);
+        }
+    }
+}

c2c0c4ccccd52395bf0092001245f907e1772d19
vundelayamini <vundelayamini@gmail.com>
2021-03-12 16:32:43 +0530

    [yamini]ADD: Enter PreDefined MobileFormat

--- PreDefinedPassWord/Program.cs
+++ PreDefinedPassWord/Program.cs
@@ -1,23 +1,25 @@
 ﻿using System;
 
-namespace UserMobileFormat
+namespace PreDefinedPassWord
 {
     class Program
     {
         static void Main(string[] args)
         {
             string firstName = "Yamini";
-            Console.WriteLine("First name result :" + result);
+            Console.WriteLine("First name result  is :" + result);
 
             string lastName = "mahesh";
-            Console.WriteLine("Last name result :" + result);
+            Console.WriteLine("Last name result is :" + result);
 
             string emial = "vudelayamini@gmail.com";
-            Console.WriteLine("Email result :" + result);
+            Console.WriteLine("Email result is :" + result);
 
             string phoneNumber = "91 9014066209";
             Console.WriteLine("phoneNumber result is :" + result);
 
+            string password = "yaminimahi"; 
+            Console.WriteLine("password result is :" + result);
         }
     }
 }
--- PreDefinedPassWord/Regex.cs
+++ PreDefinedPassWord/Regex.cs
@@ -4,14 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace UserMobileFormat
+namespace PreDefinedPassWord
 {
     class Regex
     {
         public const string Regex_FirstName = "^[A-Z]{1}[A-Za-z]{2,}$";
         public const string Regex_LastName = "^[A-Z]{1}[A-Za-z]{2,}$";
         public const string Regex_Email = "^[0-9A-Za-z]+([._+-][0-9A-Za-z]+)*[@][0-9A-Za-z]+.[a-zA-Z]{2,3}(.[a-zA-Z]{2})?$";
-       public const string Regex_phoneNumber = "^[1-9]{1}[0-9]{0,3}\\s[1-9]{1}[0-9]{9}$";
+        public const string Regex_phoneNumber = "^[1-9]{1}[0-9]{0,3}\\s[1-9]{1}[0-9]{9}$";
+        public const string Regex_Password = "[\\S]{8,}";
         public bool ValidateFirstName(string firstName)
         {
             return Regex.IsMatch(firstName, Regex_FirstName);
@@ -28,7 +29,10 @@ namespace UserMobileFormat
         {
             return Regex.IsMatch(phoneNumber, Regex_phoneNumber);
         }
-
+        public bool ValidatePassword(string password)
+        {
+            return Regex.IsMatch(password, Regex_Password);
+        }
 
     }
 }

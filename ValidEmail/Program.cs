6681216930a63b77d632a21dd873dacee3ea1a73
vundelayamini <vundelayamini@gmail.com>
2021-03-12 15:50:59 +0530

    [yamini]ADD: Enter valid Email

---
+++ ValidEmail/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace ValidEmail
+{
+    class Program
+    {
+       public static void Main(string[] args)
+       {
+            string firstName = "Yamini";
+            Console.WriteLine("First name result :" + result);
+
+            string lastName = "mahesh";
+            Console.WriteLine("Last name result :" + result);
+
+            string emial = "vudelayamini@gmail.com";
+            Console.WriteLine("Email result :" + result);
+
+            
+           
+       }
+    }
+}

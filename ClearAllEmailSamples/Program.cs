822b5826fbc2dd968ee16d946780523ab558217e
vundelayamini <vundelayamini@gmail.com>
2021-03-12 18:29:16 +0530

    [yamini]ADD: Clear All sample Email Samples

---
+++ ClearAllEmailSamples/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace ClearAllEmailSamples
+{
+    class Program
+    {
+        private static string result;
+
+        static void Main(string[] args)
+        {
+            string frstName = "Yamini";
+            Console.WriteLine("First name result  is :" + result);
+
+            string LastName = "mahesh";
+            Console.WriteLine("Last name result is :" + result);
+
+            string emial = "vudelayamini@gmail.com";
+            Console.WriteLine("Email result is :" + result);
+
+            string phoneNumber = "91 9014066209";
+            Console.WriteLine("phoneNumber result is :" + result);
+            Console.WriteLine("password result is :" + result);
+
+            //Validating Sample Emails
+            List<string> sampleEmailList = new List<string>()
+            {
+                "abc@yahoo.com",
+                "abc-100@yahoo.com",
+                "abc.100@yahoo.com",
+                "abc111@abc.com",
+                "abc-100@abc.net",
+                "abc.100@abc.com.au",
+                "abc@1.com",
+                "abc@gmail.com.com",
+                "abc+100@gmail.com"
+            };
+             foreach (string item in sampleEmailList)
+              {
+
+                if (Regex.validateEmail)
+                {
+                    Console.WriteLine("Valid Email");
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Email");
+                }
+            }
+
+        }
+    }
+}
--- ClearAllEmailSamples/Regex.cs
+++ ClearAllEmailSamples/Regex.cs
@@ -2,37 +2,37 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
-namespace UserPassWordForRule4
+namespace ClearAllEmailSamples
 {
     class Regex
     {
         public const string REGEX_FIRSTNAME = "^[A-Z]{1}[A-Za-z]{2,}$";
         public const string REGEX_LASTNAME = "^[A-Z]{1}[A-Za-z]{2,}$";
-        public const string REGEX_EMAIL = "^[0-9A-Za-z]+([._+-][0-9A-Za-z]+)*[@][0-9A-Za-z]+.[a-zA-Z]{2,3}(.[a-zA-Z]{2})?$";
-        public const string REGEX_MOBILENUMBER = "^[1-9]{1}[0-9]{0,3}\\s[1-9]{1}[0-9]{9}$";
+        public const string REGEX_EMAIL = "^[0-9A-Za-z]+([._+-][0-9A-Za-z]+)*[@][0-9A-Za-z]+.[a-zA-Z]{2,3}(.[a-zA-Z]{2,3})?$";
+        public const string REGEX_PHONENUMBER= "^[1-9]{1}[0-9]{0,3}\\s[1-9]{1}[0-9]{9}$";
         //  \S matches any non-whitespace character
         public const string REGEX_PASSWORD = "^(?=.*[A-Z])(?=.*\\d)(?=[\\w]*[\\W][\\w]*$)[\\S]{8,}$";
         public bool ValidateFirstName(string firstName)
         {
-            return Regex.IsMatch(firstName, REGEX_FIRSTNAME);
+            return Regex.Equals(firstName, REGEX_FIRSTNAME);
         }
         public bool ValidateLastName(string lastName)
         {
-            return Regex.IsMatch(lastName, REGEX_LASTNAME);
+            return Regex.Equals(lastName, REGEX_LASTNAME);
         }
         public bool ValidateEmail(string email)
         {
-            return Regex.IsMatch(email, REGEX_EMAIL);
+            return Regex.Equals(email, REGEX_EMAIL);
         }
-        public bool ValidateMobile(string mobileNum)
+        public bool ValidateMobile(string phoneNumber)
         {
-            return Regex.IsMatch(mobileNum, REGEX_MOBILENUMBER);
+            return Regex.Equals(phoneNumber, REGEX_PHONENUMBER);
         }
         public bool ValidatePassword(string password)
         {
-            return Regex.IsMatch(password, REGEX_PASSWORD);
+            return Regex.Equals(password, REGEX_PASSWORD);
         }
     }
-}
 }

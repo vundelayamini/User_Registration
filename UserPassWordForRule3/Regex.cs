d4f3b94d84eca63b2354003b42a69d8bf1044401
vundelayamini <vundelayamini@gmail.com>
2021-03-15 15:02:35 +0530

    [yamini]ADD: Using TestCase Validating PassWord ForRule3

---
+++ UserPassWordForRule3/Regex.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserPassWordForRule3
+{
+    class Regex
+    {
+        public const string REGEX_FIRSTNAME = "^[A-Z]{1}[A-Za-z]{2,}$";
+        public const string REGEX_LASTNAME = "^[A-Z]{1}[A-Za-z]{2,}$";
+        public const string REGEX_EMAIL = "^[0-9A-Za-z]+([._+-][0-9A-Za-z]+)*[@][0-9A-Za-z]+.[a-zA-Z]{2,3}(.[a-zA-Z]{2})?$";
+        public const string REGEX_MOBILENUMBER = "^[1-9]{1}[0-9]{0,3}\\s[1-9]{1}[0-9]{9}$";
+        public const string REGEX_PASSWORD = "^(?=.*[A-Z])(?=.*\\d)[\\S]{8,}$";
+ 
+        public bool ValidateFirstName(string firstName)
+        {
+            return Regex.IsMatch(firstName, REGEX_FIRSTNAME);
+        }
+
+        private static bool IsMatch(string firstName, string rEGEX_FIRSTNAME)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ValidateLastName(string lastName)
+        {
+            return Regex.IsMatch(lastName, REGEX_LASTNAME);
+        }
+        public bool ValidateEmail(string email)
+        {
+            return Regex.IsMatch(email, REGEX_EMAIL);
+        }
+        public bool ValidateMobile(string mobileNum)
+        {
+            return Regex.IsMatch(mobileNum, REGEX_MOBILENUMBER);
+        }
+        public bool ValidatePassword(string password)
+        {
+            return Regex.IsMatch(password, REGEX_PASSWORD);
+        }
+    }
+}

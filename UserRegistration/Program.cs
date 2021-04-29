49b4264bf795d1dc636ebb80014cdba037dde8bd
vundelayamini <vundelayamini@gmail.com>
2021-04-29 17:27:06 +0530

    [yamini]Add : clear all email samples

--- UserRegistration/Program.cs
+++ UserRegistration/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace UserRegistration
 {
@@ -24,6 +25,30 @@ namespace UserRegistration
             Console.WriteLine("Enter Your Password");//take input from the user
             string Password = Console.ReadLine();//store the input for password
             Console.WriteLine(regexValidation.ValidatePassword(Password));
+
+            List<string> sampleEmailList = new List<string>()
+            {
+                "ami@ygmail.com",
+                "ami-100@yahoo.com",
+                "ami.100@abcd.com",
+                "amit111@amit.com",
+                "ami-100@gmail.net",
+                "amit.100@abc.com.au",
+                "aamit@1.com",
+                "amitc@gmail.com.com",
+                "amit+100@gmail.com"
+            };
+            foreach (string item in sampleEmailList)
+            {
+                if (regexValidation.ValidateEmail(item))
+                {
+                    Console.WriteLine("Valid Email");
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Email");
+                }
+            }
         }
     }
 }

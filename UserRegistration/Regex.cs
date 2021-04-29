867f62ab5f513fc4c83a310ddcc35feb5033af97
vundelayamini <vundelayamini@gmail.com>
2021-04-29 12:10:00 +0530

    [yamini]Add: validating the first name

--- UserRegistration/Regex.cs
+++ UserRegistration/Regex.cs
@@ -1,101 +1,22 @@
-﻿using System;
+﻿using Microsoft.VisualStudio.Services.Users;
+using System;
 using System.Collections.Generic;
 using System.Runtime.Serialization;
 using System.Text;
 
 namespace UserRegistration
 {
-    public class Regex 
-    { 
-       public const string REGEX_FIRSTNAME = "^[A-Z]{1}[A-Za-z]{2,}$";
-       public const string REGEX_LASTNAME = "^[A-Z]{1}[A-Za-z]{2,}$";
-       public const string REGEX_EMAIL = "^[0-9A-Za-z]+([._+-][0-9A-Za-z]+)*[@][0-9A-Za-z]+.[a-zA-Z]{2,3}(.[a-zA-Z]{2,3})?$";
-       public const string REGEX_MOBILENUMBER = "^[1-9]{1}[0-9]{0,3}\\s[1-9]{1}[0-9]{9}$";
-       //  \S matches any non-whitespace character
-      public const string REGEX_PASSWORD = "^(?=.*[A-Z])(?=.*\\d)(?=[\\w]*[\\W][\\w]*$)[\\S]{8,}$";
+    public class Regex
+    {
+        public const string REGEX_FIRSTNAME = "^[A-Z]{1}[A-Za-z]{2,}$";
 
-        public bool ValidateLastName(string lastName)
+        public bool ValidateLastName(string firstName)
         {
-            return (Regex.Equals(lastName, REGEX_LASTNAME));
-        }
-
-        public bool ValidateEmail(string email)
-        {
-            return (Regex.Equals(email, REGEX_EMAIL));
-        }
-
-        public bool ValidateMobile(string mobNumber)
-        {
-            return (Regex.Equals(mobNumber, REGEX_MOBILENUMBER));
-        }
-
-        public bool ValidatePassword(string password)
-        {
-            return (Regex.Equals(password, REGEX_PASSWORD));
-        }
-
-        public bool ValidateLastName(string LastName)
-        {
-            try
-            {
-                if (Regex.Equals(LastName, REGEX_LASTNAME))
-                    return true;
-                else
-                    throw new UserException(UserException.ExceptionType.INVALID_LASTNAME, "Sorry Invalid Last Name");
-            }
-            catch (UserDetailsExeption ex)
-            {
-                Console.WriteLine(ex.Message);
-                return false;
-            }
-        }
-        public bool ValidateEmail(string Email)
-        {
-            try
-            {
-                if (Regex.Equals(Email, REGEX_EMAIL))
-                    return true;
-                else
-                    throw new UsersExeption(UserException.ExceptionType.INVALID_EMAIL, "Sorry Invalid Email");
-            }
-            catch (UserException ex)
-            {
-                Console.WriteLine(ex.Message);
-                return false;
-            }
-        }
-        public bool ValidateMobile(string MobileNumber)
-        {
-            try
-            {
-                if (Regex.Equals(MobileNumber, REGEX_MOBILENUMBER))
-                    return true;
-                else
-                    throw new UserException(UserException.ExceptionType.INVALID_MOBILE, "Sorry Invalid Mobile Number");
-            }
-            catch (UserException ex)
-            {
-                Console.WriteLine(ex.Message);
-                return false;
-            }
-        }
-        public bool ValidatePassword(string Password)
-        {
-            try
-            {
-                if (Equals(Password, REGEX_PASSWORD))
-                    return true;
-                else
-                    throw new UserException(UserException.ExceptionType.INVALID_PASSWORD, "Soory Invalid Password");
-            }
-            catch (UserException ex)
-            {
-                Console.WriteLine(ex.Message);
-                return false;
-            }
+            return (Regex.Equals(firstName, REGEX_FIRSTNAME));
         }
     }
+}
+        
 
-   
-    }
-    
+       
+       
--- UserRegistrationTest/UnitTest1.cs
+++ UserRegistrationTest/UnitTest1.cs
@@ -7,122 +7,18 @@ namespace UserRegistrationTest
     [TestClass]
     public class UnitTest1
     {
-        private Regex user;
-
-        [TestInitialize]
-        public void InitClassObject()
-        {
-            //Arrange
-            user = new Regex();
-        }
+       
         [TestMethod]
         public void GivenUserFistName_WhenValidate_ShouldReturnTrue()
         {
-            string firstName = "Yamini";
-            //Act
-            bool result = user.ValidateFirstName(firstName);
-            //Assert
-            Assert.IsTrue(result);
-        }
-        [TestMethod]
-        public void GivenUserLastName_WhenValidate_ShouldReturnTrue()
-        {
-            string lastName = "Mahi";
-            //Act
-            bool result = user.ValidateFirstName(lastName);
-            //Assert
-            Assert.IsTrue(result);
-        }
-        [TestMethod]
-        public void GivenEmailId_WhenValidate_ShouldReturnTrue()
-        {
-            string email = "yamini@gmail.com";
-            //Act
-            bool result = user.ValidateEmail(email);
-            //Assert
-            Assert.IsTrue(result);
-        }
-        [TestMethod]
-        public void GivenMobileNumber_WhenValidate_ShouldReturnTrue()
-        {
-            string mobileNumber = "91 9493499430";
-            //Act
-            bool result = user.ValidateMobile(mobileNumber);
-            //Assert
-            Assert.IsTrue(result);
-        }
-        [TestMethod]
-        public void GivenPassword_WhenValidate_ShouldReturnTrue()
-        {
             //Arrange
-
-            string password = "yaminimahi";
+            Regex regex = new Regex("Validate firstname");
+            string FirstName = "Amit";
             //Act
-            bool result = user.ValidatePassword(password);
+            bool result = regex.ValidateFirstName(FirstName);
             //Assert
             Assert.IsTrue(result);
-        }
-        //Sad Test Cases(Test Cases Fail The Entry)
-        [TestMethod]
-        public void GivenUserFistName_WhenValidate_ShouldReturnFalse()
-        {
-            string firstName = "yamini";
-            //Act
-            bool result = user.ValidateFirstName(firstName);
-            //Assert
-            Assert.IsFalse(result);
-        }
-        [TestMethod]
-        public void GivenUserLastName_WhenValidate_ShouldReturnFalse()
-        {
-            string lastName = "Mahi";
-            //Act
-            bool result = user.ValidateFirstName(lastName);
-            //Assert
-            Assert.IsFalse(result);
-        }
-        [TestMethod]
-        public void GivenEmailId_WhenValidate_ShouldReturnFalse()
-        {
-            string email = "yaminivundela@gmail.com";
-            //Act
-            bool result = user.ValidateEmail(email);
-            //Assert
-            Assert.IsFalse(result);
-        }
-        [TestMethod]
-        public void GivenMobileNumber_WhenValidate_ShouldReturnFalse()
-        {
-            string mobileNumber = "91 9014066209";
-            //Act
-            bool result = user.ValidateMobile(mobileNumber);
-            //Assert
-            Assert.IsFalse(result);
-        }
-        [TestMethod]
-        public void GivenPassword_WhenValidate_ShouldReturnFalse()
-        {
-            string password = "yaminimahi*124";
-            //Act
-            bool result = user.ValidatePassword(password);
-            //Assert
-            Assert.IsFalse(result);
-        }
-        [TestMethod]
-        [DataRow("abc@yahoo.com")]
-        [DataRow("abc-100@yahoo.com")]
-        [DataRow("abc.100@yahoo.com")]
-        [DataRow("abc111@abc.com")]
-        [DataRow("abc.100@abc.com.au")]
-        [DataRow("abc-100@abc.net")]
-        [DataRow("abc@1.com")]
-        [DataRow("abc@gmail.com.com")]
-        [DataRow("abc+100@gmail.com")]
-        public void ValidateEmailId_Should_return_true(string email)
-        {
-            Assert.IsTrue(user.ValidateEmail(email));
         }
     }
-
-  
+    
 }
